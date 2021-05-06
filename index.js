a213643c0a69953c440b572149da882f534c0e78
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-05-06 13:20:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 // Kata 1 
 
 const add = n => Object.assign(i => add(i + n), { valueOf: () => n }); 
+
+// Kata 2
+
+function anagramDifference(str1, str2) {
+  let count1 = Array(26).fill(0);
+  let count2 = Array(26).fill(0);
+  let i = 0;
+  while (i < str1.length) {
+    count1[str1[i].charCodeAt() - 97] += 1;
+    i += 1;
+  }
+  i = 0;
+  while (i < str2.length) {
+    count2[str2[i].charCodeAt() - 97] += 1;
+    i += 1;
+  }
+  let result = 0;
+  for (let i = 0; i < 26; i++) {
+    result += Math.abs(count1[i] - count2[i]);
+  }
+  return result;
+}; 
