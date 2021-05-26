ffcbd2c98c1e01d396a7931b23adaab617c5af1e
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-05-26 12:53:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -200,3 +200,18 @@ Math.ceil = function(number) {
 Math.floor = function(number) {
   return parseInt(number);
 };
+
+  
+  // kata 14 
+  
+  function humanReadable(seconds) {
+  var hours = parseInt(seconds / 3600);
+  var minutes = parseInt(seconds / 60) % 60;
+  var seconds = seconds % 60;
+
+  var pad = function(val) {
+    return val < 10 ? "0" + val : val;
+  };
+
+  return pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+};
