df8400df62ff286d98384001c115244b5361fc42
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-05-27 11:31:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -215,3 +215,16 @@ Math.floor = function(number) {
 
   return pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
 };
+
+  // kata 15
+  
+  let maxSequence = function(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++)
+    for (let j = 1; j <= arr.length; j++) {
+      if (arr.slice(i, j).reduce((a, b) => a + b, 0) > sum) {
+        sum = arr.slice(i, j).reduce((a, b) => a + b, 0);
+      }
+    }
+  return sum > 0 ? sum : 0;
+};
