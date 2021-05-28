aa49ec5f83f035a86213f4f38cf5410ebaf85a12
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-05-28 11:02:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -228,3 +228,20 @@ Math.floor = function(number) {
     }
   return sum > 0 ? sum : 0;
 };
+
+  // kata 16 
+  
+  function parseMolecule(formula) {
+  const nums = {};
+  (function reduce(molecule, mult) {
+    molecule.match(/[A-Z][a-z]?\d*|\([^\)]+\)\d*|\[[^\]]+\]\d*|{[^}]+}\d*/g).forEach(function(val) {
+      if (val.match(/\[|\]|\(|\)|{|}/g)) {
+        reduce(val.replace(/^(\[|\()|((\]|\)|{|})\d*)$/g, ''), mult*val.match(/\d*$/g)[0]||1);
+      } else {
+        const element = val.replace(/\d/g, '');
+        nums[element] = (nums[element] || 0) + (val.replace(/[A-Za-z]/g, '')||1) * mult;
+      }
+    });
+  }(formula, 1));
+  return nums;
+}; 
