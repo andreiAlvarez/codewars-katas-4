5793574fd7e5ec80b3c7742ee292091da0c869cc
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-06-02 14:30:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -260,3 +260,17 @@ Math.floor = function(number) {
   Math.min(
     ...Object.keys(needs).map(key => Math.floor(has[key] / needs[key] || 0))
   );
+
+  
+  // kata 19 
+  
+  const chooseBestSum = (t, k, ls) =>
+  ls
+    .reduce(
+      (r, e) => r.concat(r.filter(c => c.length < k).map(c => c.concat([e]))),
+      [[]]
+    )
+    .filter(c => c.length === k)
+    .map(c => c.reduce((a, b) => a + b))
+    .filter(s => s <= t)
+    .sort((a, b) => b - a)[0] || null;
