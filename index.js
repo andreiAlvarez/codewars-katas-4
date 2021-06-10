08e8661fa7170154343191e848cb412343c4cfaf
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-06-10 18:00:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -292,3 +292,7 @@ Math.floor = function(number) {
 const partitionOn = (pred, items) =>
   ((f, t) => (items.splice(0, items.length, ...f, ...t), f.length))
   (items.filter(val => !pred(val)), items.filter(pred));
+
+  // kata 23
+  
+  let lastDigit = (str1, str2) => +!+str2 || Math.pow(str1.slice(-1), str2.slice(-2) % 4 || 4) % 10;
